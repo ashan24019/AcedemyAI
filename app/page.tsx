8e2679bcc608584f07e90df7fea8fb21acd94bd4
Ashan <ashanvimodh241@gmail.com>
2025-06-02 13:51:58 +0530

    Cmpanion list and CTA done

--- app/page.tsx
+++ app/page.tsx
@@ -2,45 +2,50 @@ import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
 import React from "react";
+import {recentSessions} from "@/constants";
 
 const Page = () => {
-  return (
-    <main>
-      <h1 className="text-2xl underline">Popular Companions</h1>
-      <section className="home-section">
-        <CompanionCard 
-           id = "123"
-           name = "Neura the Brainy Explorer"
-           topic = "Neural Network of the Brain"
-           subject = "Science"
-           duration = {45}
-           color = "#E5D0FF"
-        />
-        <CompanionCard 
-          id = "456"
-          name = "Countsy the Number Wizard"
-          topic = "Derivatives & Integrals"
-          subject = "Maths"
-          duration = {30}
-          color = "#FFDA6E"
-        />
-        <CompanionCard 
-          id = "789"
-          name = "Verba the Vocabulary Builder"
-          topic = "English Literature"
-          subject = "Language"
-          duration = {30}
-          color = "#BDE7FF"
+    return (
+        <main>
+            <h1 className="text-2xl underline">Popular Companions</h1>
+            <section className="home-section">
+                <CompanionCard
+                    id="123"
+                    name="Neura the Brainy Explorer"
+                    topic="Neural Network of the Brain"
+                    subject="Science"
+                    duration={45}
+                    color="#E5D0FF"
+                />
+                <CompanionCard
+                    id="456"
+                    name="Countsy the Number Wizard"
+                    topic="Derivatives & Integrals"
+                    subject="Maths"
+                    duration={30}
+                    color="#FFDA6E"
+                />
+                <CompanionCard
+                    id="789"
+                    name="Verba the Vocabulary Builder"
+                    topic="English Literature"
+                    subject="Language"
+                    duration={30}
+                    color="#BDE7FF"
 
-        />
-      </section>
+                />
+            </section>
 
-      <section className="home-section">
-        <CompanionsList />
-        <CTA />
-      </section>
-    </main>
-  );
+            <section className="home-section">
+                <CompanionsList
+                    title="Recently completed session"
+                    companions = {recentSessions}
+                    classNames="w-2/3 max-lg:w-full"
+                />
+                <CTA/>
+            </section>
+        </main>
+    );
 };
 
 export default Page;
