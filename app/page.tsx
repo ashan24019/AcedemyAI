58ebcca02ed0bcc159dcb053b2dcbc666ce78ce6
Ashan <ashanvimodh241@gmail.com>
2025-06-11 12:48:07 +0530

    session history done

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,38 @@
 import CompanionCard from "@/components/CompanionCard";
 import CompanionsList from "@/components/CompanionsList";
 import CTA from "@/components/CTA";
-import React from "react";
-import {recentSessions} from "@/constants";
+import {getAllCompanions, getRecentSessions} from "@/lib/actions/companion.actions";
+import {getSubjectColor} from "@/lib/utils";
 
-const Page = () => {
-    return (
-        <main>
-            <h1 className="text-2xl underline">Popular Companions</h1>
-            <section className="home-section">
+const Page = async () => {
+    const companions = await getAllCompanions({ limit: 3 });
+    const recentSessionsCompanions = await getRecentSessions(10);
+
+  return (
+    <main>
+      <h1>Popular Companions</h1>
+
+        <section className="home-section">
+            {companions.map((companion) => (
                 <CompanionCard
-                    id="123"
-                    name="Neura the Brainy Explorer"
-                    topic="Neural Network of the Brain"
-                    subject="Science"
-                    duration={45}
-                    color="#E5D0FF"
+                    key={companion.id}
+                    {...companion}
+                    color={getSubjectColor(companion.subject)}
                 />
-                <CompanionCard
-                    id="456"
-                    name="Countsy the Number Wizard"
-                    topic="Derivatives & Integrals"
-                    subject="Maths"
-                    duration={30}
-                    color="#FFDA6E"
-                />
-                <CompanionCard
-                    id="789"
-                    name="Verba the Vocabulary Builder"
-                    topic="English Literature"
-                    subject="Language"
-                    duration={30}
-                    color="#BDE7FF"
+            ))}
 
-                />
-            </section>
+        </section>
 
-            <section className="home-section">
-                <CompanionsList
-                    title="Recently completed session"
-                    companions = {recentSessions}
-                    classNames="w-2/3 max-lg:w-full"
-                />
-                <CTA/>
-            </section>
-        </main>
-    );
-};
+        <section className="home-section">
+            <CompanionsList
+                title="Recently completed sessions"
+                companions={recentSessionsCompanions}
+                classNames="w-2/3 max-lg:w-full"
+            />
+            <CTA />
+        </section>
+    </main>
+  )
+}
 
-export default Page;
+export default Page
